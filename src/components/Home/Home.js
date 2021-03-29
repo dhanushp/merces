c0156dd24e1073b265e17889330016e48cd8dfac
dhanushp <dhanushp.work@gmail.com>
2021-03-29 14:28:48 +0530

    Update

---
+++ src/components/Home/Home.js
@@ -0,0 +1,146 @@
+import React from "react"
+import styles from "./styles.module.scss"
+import Container from "../Container/Container"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+//SVG
+import Warranty from "../../images/Warranty.svg"
+import Installation from "../../images/Installation.svg"
+import Standard from "../../images/Standard.svg"
+
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      header: file(relativePath: { eq: "header.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      faucet: file(relativePath: { eq: "Faucet.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      sanitary: file(relativePath: { eq: "Sanitaryware.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Container>
+      <section className={styles.header}>
+        <div className={styles.headerText}>
+          <h1 className={styles.headerTextMain}>Merces -</h1>
+          <h1 className={styles.headerTextMain}>Elegance Meets Quality</h1>
+          <p className={styles.headerTextSecondary}>
+            With the help of Modern Engineering and Craftmanship Merces provides
+            the best quality proudct with USA standard as well as Elegance.
+          </p>
+        </div>
+        <div className={styles.headerImage}>
+          <Img
+            className={styles.heroImageRef}
+            fluid={data.header.childImageSharp.fluid}
+            alt="Header"
+          />
+        </div>
+      </section>
+      <section className={styles.service}>
+        <div className={styles.serviceHeading}>
+          <h1 className={styles.serviceHeadingPrimary}>Services</h1>
+          <h1 className={styles.serviceHeadingSecondary}>
+            Create a Customer Relation that Last Forever
+          </h1>
+        </div>
+        <div className={styles.serviceLists}>
+          <div className={styles.serviceList}>
+            <div className={styles.serviceListImage}>
+              <img
+                src={Warranty}
+                className={styles.serviceListImage_Image}
+                alt="Warranty"
+              ></img>
+            </div>
+            <h1 className={styles.serviceListHeading}>10 Years Warranty</h1>
+            <p className={styles.serviceListDetail}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Libero
+              sapien morbi quis bibendum. Proin commodo faucibus duis hendrerit
+              leo, blandit pulvinar ante.{" "}
+            </p>
+          </div>
+          <div className={styles.serviceList}>
+            <div className={styles.serviceListImage}>
+              <img
+                src={Installation}
+                className={styles.serviceListImage_Image}
+                alt="Installation"
+              ></img>
+            </div>
+            <h1 className={styles.serviceListHeading}>Easy Installation</h1>
+            <p className={styles.serviceListDetail}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Libero
+              sapien morbi quis bibendum. Proin commodo faucibus duis hendrerit
+              leo, blandit pulvinar ante.{" "}
+            </p>
+          </div>
+          <div className={styles.serviceList}>
+            <div className={styles.serviceListImage}>
+              <img
+                src={Standard}
+                className={styles.serviceListImage_Image}
+                alt="Standard"
+              ></img>
+            </div>
+            <h1 className={styles.serviceListHeading}>USA Standard Product</h1>
+            <p className={styles.serviceListDetail}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Libero
+              sapien morbi quis bibendum. Proin commodo faucibus duis hendrerit
+              leo, blandit pulvinar ante.{" "}
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.product}>
+        <div className={styles.productHeading}>
+          <h1 className={styles.productHeadingPrimary}>Products</h1>
+          <h1 className={styles.productHeadingSecondary}>
+            Explore the Wide Range of Quality Products
+          </h1>
+        </div>
+        <div className={styles.productDetail}>
+          <div className={styles.productDetailList}>
+            <div className={styles.productDetailImage}>
+              <Img
+                className={styles.productDetailImage_Image}
+                fluid={data.faucet.childImageSharp.fluid}
+                alt="Faucets"
+              />
+            </div>
+            <h1 className={styles.productDetailListHeading}>Faucets</h1>
+          </div>
+          <div className={styles.productDetailList}>
+            <div className={styles.productDetailImage}>
+              <Img
+                className={styles.productDetailImage_Image}
+                fluid={data.sanitary.childImageSharp.fluid}
+                alt="Sanitaryware"
+              />
+            </div>
+            <h1 className={styles.productDetailListHeading}>Sanitarywares</h1>
+          </div>
+        </div>
+      </section>
+    </Container>
+  )
+}
+
+export default Home
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,39 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  const handleScoll = () => {
+    const offset = window.scrollY
+
+    if (offset > 50) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScoll)
+  })
+
+  let header = ["header"]
+  if (scrolled) {
+    header.push(" scrolled")
+  }
+
+  let text = ["header__logo-text"]
+  if (scrolled) {
+    text.push(" scroll")
+  }
+
+  return (
+    <div className={header.join("")}>
+      <div className="header__logo">
+        <h1 className={text.join("")}>Merces</h1>
+      </div>
     </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  )
 }
 
 export default Header
